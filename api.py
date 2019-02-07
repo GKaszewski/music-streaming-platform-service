f533b7aaa8846f3a2c36bc2a867ec7538e9e00bb
GKaszewski <gabrielkaszewski@gmail.com>
2019-02-07 23:14:38 +0100

    init

---
+++ api.py
@@ -0,0 +1,25 @@
+class Song:
+    albumName = ""
+    albumUrl = ""
+    author = ""
+    songUrl = ""
+    tags = []
+    title = ""
+
+    def __init__(self, albumName, albumUrl, author, songUrl, tags, title):
+        self.title = title
+        self.tags = tags
+        self.songUrl = songUrl
+        self.author = author
+        self.albumUrl = albumUrl
+        self.albumName = albumName
+
+    def create(songObj):
+        newSong = Song(songObj.child('albumName').get(), songObj.child('albumUrl').get(), songObj.child('author').get(), songObj.child('songUrl').get(), songObj.child('tags').get(), songObj.child('title').get())
+        return newSong
+
+    def getSongUrl(self):
+        return self.songUrl
+
+    def getAlbumUrl(self):
+        return self.albumUrl
---
+++ backend.py
@@ -0,0 +1,29 @@
+from flask import Flask, Response, render_template, Markup, jsonify
+import markdown, os
+import firebase_admin as fb
+from firebase_admin import db
+from api import Song
+
+app = Flask(__name__)
+
+#Firebase database configuration
+credsPath = os.path.join(app.root_path, 'creds.json')
+creds = fb.credentials.Certificate(credsPath)
+firebaseApp = fb.initialize_app(creds, {
+    "databaseURL": 'https://musicstreamingplatform.firebaseio.com/'
+})
+##
+
+
+@app.route("/")
+def index():
+    with open("readme.md", 'r') as markdown_file:
+        content = markdown_file.read()
+        page = Markup(markdown.markdown(content))
+        return page
+
+@app.route("/song/<title>")
+def getSong(title):
+    songObj = fb.db.reference('/{}'.format(title)).child('0')
+    songRef = Song.create(songObj)
+    return jsonify(albumName = songRef.albumName, albumUrl = songRef.albumUrl, author = songRef.author, songUrl = songRef.songUrl, tags = songRef.tags, title = songRef.title)  
