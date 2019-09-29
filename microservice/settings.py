e6f4b12185bf22c0fee6094e755793bc5fdd3da1
GKaszewski <gabrielkaszewski@gmail.com>
2019-09-30 00:28:33 +0200

    added heroku to settings file

--- microservice/settings.py
+++ microservice/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-import os
+import os, django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
